259f8f56be92a2a4c5f10674f26ccbf9f8b5cb46
MrDemiurgos <93350505+Davidlopes99@users.noreply.github.com>
2023-09-24 09:27:51 -0300

    Lógica de Login de CPF feita.

---
+++ src/ClassPage.jsx
@@ -0,0 +1,153 @@
+import { FlatList, View, StyleSheet, Text, Pressable, Dimensions } from 'react-native';
+import AppBar from './AppBar';
+
+const windowWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+        flexShrink: 1,
+    },
+    separator: {
+        marginVertical: 5,
+    },
+    flexCard: {
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        height: 50,
+        gap: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: windowWidth - 20,
+        borderStyle: 'solid',
+        borderColor: 'grey',
+        borderWidth: StyleSheet.hairlineWidth, 
+        borderRadius: 5, 
+    },
+    nameStyle: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 13,
+    },
+    classHeaderStyle: {
+        marginTop: 20,
+        marginBottom: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center', 
+        fontWeight: 'bold',
+        fontSize: 17
+    },
+    dateHeaderStyle: {
+        marginBottom: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold'
+    },
+    presenteButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'blue',
+        width: 65,
+        height: 25,
+        borderRadius: 5,
+    },
+    ausenteButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red',
+        width: 65,
+        height: 25,
+        borderRadius: 5,
+        marginRight: 5
+    }
+})
+
+const getCurrentDate = () => {
+ 
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    //Alert.alert(date + '-' + month + '-' + year);
+    // You can turn it in to your desired format
+    return date + '/' + month + '/' + year;//format: d-m-y;
+}
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
+const ListHeader = ({ className }) => {
+    return (
+        <View>
+            <Text style={styles.classHeaderStyle}>{className}</Text>
+            <Text style={styles.dateHeaderStyle}>{getCurrentDate()}</Text>
+        </View>
+    )
+}
+
+const onPressCard = ({ student }) => {
+    console.log(JSON.stringify({student}));
+}
+
+const onPressPresente=({ student }) => {
+    console.log(JSON.stringify({student})+' Presente');
+}
+
+
+const onPressAusente=({ student }) => {
+    console.log(JSON.stringify({student})+' Ausente');
+}
+
+const StudentCard = ({ student }) => {
+    return (
+
+        <Pressable onPress={() => onPressCard({student})}>
+            <View style={styles.flexCard}>
+                <Text style={styles.nameStyle}>{student}</Text>
+                <Pressable onPress={() => onPressPresente({student})} style={styles.presenteButton}>
+                    <Text style={{fontWeight: 'bold', color:'white'}}>Presente</Text>
+                </Pressable>
+                <Pressable onPress={()=> onPressAusente({student})} style={styles.ausenteButton}>
+                    <Text style={{fontWeight: 'bold', color:'white'}}>Ausente</Text>
+                </Pressable>
+            </View>
+        </Pressable>
+    )
+}
+
+const className = "TCC00293 - Engenharia de Software II";
+
+const students = ["Lexie George",
+    "Nikolas Fisher",
+    "Mayra Jackson",
+    "Jewel Watson",
+    "Alexandra Finley",
+    "Emmalee French",
+    "Andres Roth",
+    "Bailey Everett",
+    "Catalina Chaney",
+    "Elisabeth Fuentes",
+    "Deven Bishop",
+    "Cael Rosario",
+    "Christopher Smith Hartmann Fields"]
+
+const ClassPage = () => {
+
+    return (
+        <View style={styles.container}>
+            <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
+                <FlatList
+                    ListHeaderComponent={<ListHeader className={className} />}
+                    data={students}
+                    ItemSeparatorComponent={ItemSeparator}
+                    renderItem={({ item }) => <StudentCard student={item}/>}
+                />
+            </View>
+        </View>
+    );
+}
+
+export default ClassPage;
---
+++ src/Login.jsx
@@ -0,0 +1,165 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ImageBackground,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function Login() {
+  const [cpf, setCPF] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+
+  const handleCPFChange = (text) => {
+    const numericValue = text.replace(/\D/g, '');
+
+    const maxLength = 11;
+    const truncatedCPF = numericValue.slice(0, maxLength);
+
+    let formattedCPF = '';
+    for (let i = 0; i < maxLength; i++) {
+      if (i === 3 || i === 6) {
+        formattedCPF += '.';
+      } else if (i === 9) {
+        formattedCPF += '-';
+      }
+      formattedCPF += truncatedCPF.charAt(i);
+    }
+
+  setCPF(formattedCPF);
+};
+
+  const handleLogin = () => {
+    console.log('CPF:', cpf);
+    console.log('Senha:', senha);
+  };
+
+  const toggleMostrarSenha = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
+
+  return (
+    <ImageBackground
+      source={require('C:/UFF/ES2/ss/sistemaDeChamadas-main/chamadas-frontend/assets/uff-logo.png')}
+      style={styles.backgroundImage}
+    >
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
+      >
+        <View style={styles.logoContainer}>
+          <Text style={styles.loginText}>Bem-vindo à UFF</Text>
+        </View>
+        <View style={styles.formContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="CPF"
+            onChangeText={handleCPFChange}
+            value={cpf}
+            keyboardType="numeric"
+          />
+          <View style={styles.passwordContainer}>
+            <TextInput
+              style={styles.passwordInput}
+              placeholder="Senha"
+              onChangeText={(text) => setSenha(text)}
+              value={senha}
+              secureTextEntry={!mostrarSenha}
+            />
+            <TouchableOpacity onPress={toggleMostrarSenha}>
+              <Icon
+                name={mostrarSenha ? 'eye-slash' : 'eye'}
+                size={20}
+                color="blue"
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={handleLogin}
+          >
+            <Text style={styles.loginButtonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+    padding: 20, 
+  },
+  logoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20, 
+    color: '#002b5e', 
+  },
+  formContainer: {
+    flex: 2,
+    width: '80%',
+    alignItems: 'center',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingLeft: 10,
+    backgroundColor: 'white',
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 20,
+  },
+  passwordInput: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    backgroundColor: 'white',
+  },
+  loginButton: {
+    width: '100%', 
+    height: 50, 
+    backgroundColor: 'blue', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    borderRadius: 5,
+    marginTop: 20, 
+  },
+  loginButtonText: {
+    fontSize: 18,
+    color: 'white', 
+    fontWeight: 'bold',
+  },
+});
