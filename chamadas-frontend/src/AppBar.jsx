5646e9c8133e6a3e43d686a5ee4ea2c89e837b09
MrDemiurgos <93350505+Davidlopes99@users.noreply.github.com>
2023-10-18 16:35:57 -0300

    Add files via upload

--- chamadas-frontend/src/AppBar.jsx
+++ chamadas-frontend/src/AppBar.jsx
@@ -1,71 +1,114 @@
-import { Text, StyleSheet, View, ScrollView } from 'react-native';
-import React, { useState } from 'react'
-import { Link } from 'react-router-native'
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, Modal, SafeAreaView } from 'react-native';
+import { Link } from 'react-router-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const styles = StyleSheet.create({
     appBarText: {
-        color: "white",
-        //fontWeight: 700
+        color: 'white',
     },
     container: {
-        height: 40,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        gap:10
-    },
-    appBar: {
+        paddingTop: 20,
+        height: 60,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 10,
         backgroundColor: 'blue',
-        paddingLeft:5
     },
-    saveAreaViewContainer: {
-        flex: 1, 
-        backgroundColor: '#FFF'
-    },
-    viewContainer: {
-        flex: 1, 
-        backgroundColor: '#FFF'
-    },
-    scrollViewContainer: {
-        flexGrow: 1,
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingVertical: '10%',
     },
-    divider: {
-        width: 12
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-})
+    modalContent: {
+        backgroundColor: 'blue',
+        padding: 20,
+        borderRadius: 10,
+        width: 150,
+    },
+    modalOption: {
+        paddingVertical: 10,
+    },
+    selectedOptionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    arrowIcon: {
+        color: 'white',
+        marginLeft: 5,
+    },
+});
 
-const AppBarTab = ({ title, route }) => {
+const AppBarTab = ({ title, onSelect, options }) => {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const handleSelect = (option) => {
+        setModalVisible(false);
+        onSelect(option);
+    };
+
     return (
-    <Link to={route}>
-            <Text style={styles.appBarText}>{title}</Text>
-    </Link>
-    )
-}
-const appBarStyles = [ styles.container, styles.appBar ];
+        <View style={styles.selectedOptionContainer}>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={[styles.appBarText, { color: 'white' }]}>{title}</Text>
+                    <Icon name="keyboard-arrow-down" size={20} style={styles.arrowIcon} />
+                </View>
+            </TouchableOpacity>
+            <Modal
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={[styles.modalContent, { backgroundColor: 'blue' }]}>
+                        {options.map((option) => (
+                            <TouchableOpacity
+                                key={option}
+                                style={styles.modalOption}
+                                onPress={() => handleSelect(option)}
+                            >
+                                <Text style={{ color: 'white' }}>{option}</Text>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    );
+};
 
 const SignedInAppBar = () => {
+    const [selectedRole, setSelectedRole] = useState('Professor');
+    const roles = ['Professor', 'Doutorando', 'Mestrando', 'Graduando'];
+
     return (
-        <View style={appBarStyles}> 
-            <ScrollView  contentContainerStyle={{ gap: 15 }} horizontal>
-                <AppBarTab title="Aluno" route="/turmas/aluno" />
-                <AppBarTab title="Professor" route="/turmas/professor" />
-                <AppBarTab title="Sair" route="/login"/>
-            </ScrollView>
-        </View>
-    )
+        <SafeAreaView style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <AppBarTab
+                    title={selectedRole}
+                    onSelect={setSelectedRole}
+                    options={roles}
+                />
+                <Link to="/turmas/professor">
+                    <Text style={styles.appBarText}>Turmas</Text>
+                </Link>
+                <Link to="/login">
+                    <Text style={styles.appBarText}>Sair</Text>
+                </Link>
+            </View>
+        </SafeAreaView>
+    );
 };
-
-
 
 const AppBar = () => {
-
-
-    return <SignedInAppBar  />
+    return <SignedInAppBar />;
 };
-
 
 export default AppBar;
--- chamadas-frontend/src/TurmasPage.jsx
+++ chamadas-frontend/src/TurmasPage.jsx
@@ -1,6 +1,6 @@
-import { FlatList, View, StyleSheet, Text, Pressable, Dimensions } from 'react-native';
+import React from 'react';
+import { View, Text, Pressable, FlatList, StyleSheet, Dimensions } from 'react-native';
 import { useNavigate, useParams } from 'react-router-native';
-
 
 const windowWidth = Dimensions.get('screen').width;
 
@@ -23,8 +23,8 @@ const styles = StyleSheet.create({
         width: windowWidth - 20,
         borderStyle: 'solid',
         borderColor: 'lightgrey',
-        borderWidth: StyleSheet.hairlineWidth, 
-        borderRadius: 5, 
+        borderWidth: StyleSheet.hairlineWidth,
+        borderRadius: 5,
     },
     nameStyle: {
         display: 'flex',
@@ -37,21 +37,42 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center', 
-        //fontWeight: 'bold',
-        fontSize: 17
+        alignItems: 'center',
+        fontSize: 17,
     },
     dateStyle: {
         paddingRight: 5,
         marginTop: 5,
-        textAlign: "right"
-        //fontWeight: 'bold'
+        textAlign: 'right',
     },
     roleStyle: {
         paddingLeft: 5,
-        marginTop: 5
-    }
-})
+        marginTop: 5,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: windowWidth - 20,
+        justifyContent: 'space-between',
+    },
+    button: {
+        flex: 1,
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonPresenca: {
+        backgroundColor: '#836FFF', // Roxo claro
+    },
+    buttonHistorico: {
+        backgroundColor: '#00FF7F', // Verde claro
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+});
 
 const turmas = [
     {
@@ -69,39 +90,60 @@ const turmas = [
         professor: "Isabel Rosseti",
         time: "11:00 às 13:00"
     },
-]
+];
+
+const onPressButton = (action, turma, navigate, role) => {
+    if (role === 'aluno') {
+        alert(`Ação: ${action} - Turma: ${turma.name}`);
+    }
+    if (role === 'professor') {
+        if (action === 'Presença') {
+            const turmaUrl = `/turma/${encodeURIComponent(turma.name)}`;
+            navigate(turmaUrl);
+        } else {
+            navigate(`/classpage/${encodeURIComponent(turma.name)}`);
+        }
+    }
+};
 
 
-const onPressCard = ({ turma, navigate, role }) => {
-    if(role === 'professor'){
-        const turmaUrl = `/turma/${turma.name}`
-        navigate(turmaUrl);
-    }
-    if(role === 'aluno'){
-        const alunoTurmaUrl = `/aluno/${turma.name}`
-        navigate(alunoTurmaUrl)
-    }
-}
+
 const TurmaCard = ({ turma, navigate, role }) => {
-    const turmaHeader = turma.name.split("-")[0] + " - " + turma.name.split("-")[1]
-    return (
-        <Pressable onPress={() => onPressCard({ turma, navigate, role })}>
-            <View style={styles.flexCard}>
-                <Text style={styles.nameStyle}>{turmaHeader}</Text>
-            </View>
-        </Pressable>
-    )
-}
-const getCurrentDate = () => {
- 
-    var date = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var year = new Date().getFullYear();
+    const turmaHeader = turma.name.split('-')[0] + ' - ' + turma.name.split('-')[1];
 
-    //Alert.alert(date + '-' + month + '-' + year);
-    // You can turn it in to your desired format
-    return date + '/' + month + '/' + year;//format: d-m-y;
-}
+    return (
+        <View>
+            <Pressable onPress={() => onPressButton('Turma', turma, navigate, role)}>
+                <View style={styles.flexCard}>
+                    <Text style={styles.nameStyle}>{turmaHeader}</Text>
+                </View>
+            </Pressable>
+            <View style={styles.buttonContainer}>
+                <Pressable
+                    style={[styles.button, styles.buttonPresenca]}
+                    onPress={() => onPressButton('Presença', turma, navigate, role)}
+                >
+                    <Text style={styles.buttonText}>Presença</Text>
+                </Pressable>
+                <Pressable
+                    style={[styles.button, styles.buttonHistorico]}
+                    onPress={() => onPressButton('Histórico', turma, navigate, role)}
+                >
+                    <Text style={styles.buttonText}>Histórico</Text>
+                </Pressable>
+            </View>
+        </View>
+    );
+};
+
+
+
+const getCurrentDate = () => {
+    const date = new Date().getDate();
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+    return date + '/' + month + '/' + year;
+};
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
@@ -110,30 +152,29 @@ const TurmasHeader = () => {
         <View>
             <Text style={styles.turmasHeaderStyle}>Minhas turmas</Text>
         </View>
-    )
-}
+    );
+};
 
 const TurmasPage = () => {
     const navigate = useNavigate();
     const role = useParams().role;
-    const roleHeader = role.charAt(0).toUpperCase() + role.slice(1)
-    return ( 
+    const roleHeader = role.charAt(0).toUpperCase() + role.slice(1);
+    return (
         <View style={styles.container}>
-            <View style={{flexDirection:'row', justifyContent: 'space-between'}}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                 <Text style={styles.roleStyle}>{roleHeader}</Text>
                 <Text style={styles.dateStyle}>{getCurrentDate()}</Text>
             </View>
-            <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <FlatList
                     ListHeaderComponent={<TurmasHeader />}
                     data={turmas}
                     ItemSeparatorComponent={ItemSeparator}
-                    renderItem={({ item }) => <TurmaCard role={role} turma={item} navigate={navigate}/>}
+                    renderItem={({ item }) => <TurmaCard role={role} turma={item} navigate={navigate} />}
                 />
             </View>
         </View>
     );
-
-}
+};
 
 export default TurmasPage;
